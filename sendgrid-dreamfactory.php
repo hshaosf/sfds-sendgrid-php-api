60732133062b9b7595ae3df80bd47f46481d1cd5
hui <hui@TIS1SVNLTSHAOH01.local>
2018-01-12 18:10:50 -0800

    bug fixes

--- sendgrid-dreamfactory.php
+++ sendgrid-dreamfactory.php
@@ -1,12 +1,12 @@
 <?php
 
 require 'vendor/autoload.php';
-require 'src/SendGridApi.php';
+require 'src/SendgridApi.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-$sendgrid = new \SendGridApi();
+$sendgrid = new \SendgridApi();
 
 $request_body = $event['request']['payload'];
 
@@ -16,13 +16,15 @@ if($app['status_code']==200){
     $app_name = $app['content']['name'];
 }
 
-!isset($request_body['tags']) $request_body['tags'] = array();
+if(!isset($request_body['tags'])) $request_body['tags'] = array();
 array_push($request_body['tags'], 'dreamfactory '.$app_name);
 
 $response = $sendgrid->send($request_body);
-
-$event['response']['status_code'] = $response->statusCode();
-$event['response']['content'] = $response->body();
-
-
+if($response){
+  $event['response']['status_code'] = $response->statusCode();
+  $event['response']['content'] = $response->body();
+}else{
+  $event['response']['status_code'] = 500;
+  $event['response']['content'] = 'Invalid request';
+}
 
